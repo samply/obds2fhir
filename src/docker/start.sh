aac9bb96faef720a95058e10c65c29c607e750c7
p.delpy@dkfz-heidelberg.de <p.delpy@dkfz-heidelberg.de>
2022-05-30 09:02:22 +0200

    Update bash code to validate all required directories- start.sh

--- src/docker/start.sh
+++ src/docker/start.sh
@@ -1,29 +1,27 @@
 #!/usr/bin/env bash
 
-
 export FILES_TO_PARSE="/etc/samply/adt2fhir.properties"
 
 set -e
 
-for file in $FILES_TO_PARSE
-do
-sed -i "s|{file_path}|${FILE_PATH:-/adt2fhir/clinical_data}|"                                                         $file
-sed -i "s|{store_path}|${STORE_PATH:-http://host.docker.internal:8090/fhir}|"                                         $file
-sed -i "s|{identifier_system}|${IDENTIFIER_SYSTEM:-http://dktk.dkfz.de/fhir/onco/core/CodeSystem/PseudonymArtCS}|"    $file
-sed -i "s|{mainzelliste_url}|${MAINZELLISTE_URL:-http://host.docker.internal:8080}|"                                  $file
-sed -i "s|{mainzelliste_apikey}|${MAINZELLISTE_APIKEY}|"                                                              $file
+for file in $FILES_TO_PARSE; do
+  sed -i "s|{file_path}|${FILE_PATH:-/adt2fhir/clinical_data}|" $file
+  sed -i "s|{store_path}|${STORE_PATH:-http://host.docker.internal:8090/fhir}|" $file
+  sed -i "s|{identifier_system}|${IDENTIFIER_SYSTEM:-http://dktk.dkfz.de/fhir/onco/core/CodeSystem/PseudonymArtCS}|" $file
+  sed -i "s|{mainzelliste_url}|${MAINZELLISTE_URL:-http://host.docker.internal:8080}|" $file
+  sed -i "s|{mainzelliste_apikey}|${MAINZELLISTE_APIKEY}|" $file
 done
 
-if [ -d "/clinical_data/InputADT" ]; then
-	echo "Found import dir"
-	#else echo "Error - can not find dir: create dir /clinical_data/InputADT/"
-	#exit
-fi
-
-if [ -d "/clinical_data/FHIR_Patients" ]; then
-	echo "Found output dir"
-	#else echo "Error - can not find dir: create dir /clinical_data/FHIR_Patients/"
-	#exit
-fi
+echo "Checking required input and output directories..."
+directories="InputADT ADT_Patients FHIR_Patients Processed"
+for dir in $directories; do
+  if [ -d "/adt2fhir/clinical_data/$dir" ]; then
+    echo "$dir exists"
+  else
+    echo "$dir missing - creating at ./adt2fhir/clinical_data/"
+    mkdir /adt2fhir/clinical_data/$dir/
+  fi
+done
+echo -e "\u001B[32m...done\u001B[0m"
 
 java -jar /usr/local/bin/adt2fhir.jar
