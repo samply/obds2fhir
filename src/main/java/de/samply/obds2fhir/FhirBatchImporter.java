fa6133319853460ffeb2d4c678fbcc5d7e12c051
Pierre Delpy <p.delpy@dkfz-heidelberg.de>
2025-04-29 14:34:27 +0200

    feature: fix ifNoneMatch logic, apply batch import

---
+++ src/main/java/de/samply/obds2fhir/FhirBatchImporter.java
@@ -0,0 +1,57 @@
+package de.samply.obds2fhir;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.FileEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+public class FhirBatchImporter {
+
+    private static final Logger logger = LoggerFactory.getLogger(FhirBatchImporter.class);
+
+    public static void importFile(File inputFile, HttpPost httpPost) throws IOException {
+        try (CloseableHttpClient httpClient = Util.getHttpClient(Boolean.parseBoolean(System.getenv().getOrDefault("SSL_CERTIFICATE_VALIDATION","true")));) {
+            importFile(inputFile, httpClient, httpPost);
+        }
+    }
+    public static void importFile(File inputFile, CloseableHttpClient httpClient, HttpPost httpPost) throws IOException {
+        httpPost.setEntity(new FileEntity(inputFile));
+        HttpResponse response = httpClient.execute(httpPost);
+
+        String responseBody = EntityUtils.toString(response.getEntity(), StandardCharsets.UTF_8);
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode root = mapper.readTree(responseBody);
+
+        boolean hasCriticalErrors = false;
+        int index = 0;
+
+        for (JsonNode entry : root.path("entry")) {
+            String status = entry.path("response").path("status").asText();
+
+            // Accept only 200, 201, 412
+            if (!(status.startsWith("200") || status.startsWith("201") || status.startsWith("412"))) {
+                hasCriticalErrors = true;
+                String diagnostics = entry.path("response").path("outcome").path("issue").get(0).path("diagnostics").asText();
+                System.err.println("❌ Error in entry[" + index + "]: " + status + " — " + diagnostics);
+            }
+            index++;
+        }
+
+        if (hasCriticalErrors) {
+            logger.error("FHIR import: could not import file"+ inputFile.getName());
+            logger.error(EntityUtils.toString(response.getEntity(), StandardCharsets.UTF_8)+"\n");
+            inputFile.renameTo(new File(System.getenv().getOrDefault("FILE_PATH","") + Obds2fhir.ERRONEOUS + inputFile.getName()));
+        } else {
+            System.out.println("✅ Import successful for: " + inputFile.getName());
+        }
+    }
+}
