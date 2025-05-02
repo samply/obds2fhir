8a0151580a78f4a15964643dc638920ecb586499
p.delpy@dkfz-heidelberg.de <p.delpy@dkfz-heidelberg.de>
2025-05-02 10:55:28 +0200

    to squash

--- src/main/java/de/samply/obds2fhir/FhirBatchImporter.java
+++ src/main/java/de/samply/obds2fhir/FhirBatchImporter.java
@@ -9,7 +9,6 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.util.EntityUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
@@ -26,32 +25,26 @@ public class FhirBatchImporter {
     public static void importFile(File inputFile, CloseableHttpClient httpClient, HttpPost httpPost) throws IOException {
         httpPost.setEntity(new FileEntity(inputFile));
         HttpResponse response = httpClient.execute(httpPost);
-
         String responseBody = EntityUtils.toString(response.getEntity(), StandardCharsets.UTF_8);
         ObjectMapper mapper = new ObjectMapper();
         JsonNode root = mapper.readTree(responseBody);
-
         boolean hasCriticalErrors = false;
         int index = 0;
-
         for (JsonNode entry : root.path("entry")) {
             String status = entry.path("response").path("status").asText();
-
-            // Accept only 200, 201, 412
             if (!(status.startsWith("200") || status.startsWith("201") || status.startsWith("412"))) {
                 hasCriticalErrors = true;
                 String diagnostics = entry.path("response").path("outcome").path("issue").get(0).path("diagnostics").asText();
-                logger.error("❌ Error in entry[" + index + "]: " + status + " — " + diagnostics);
+                logger.error("Error in entry[" + index + "]: " + status + " — " + diagnostics);
             }
             index++;
         }
-
         if (hasCriticalErrors) {
             logger.error("FHIR import: could not import file"+ inputFile.getName());
             logger.error(EntityUtils.toString(response.getEntity(), StandardCharsets.UTF_8)+"\n");
             inputFile.renameTo(new File(System.getenv().getOrDefault("FILE_PATH","") + Obds2fhir.ERRONEOUS + inputFile.getName()));
         } else {
-            logger.debug("✅ Import successful for: " + inputFile.getName());
+            logger.debug("Import successful for: " + inputFile.getName());
         }
     }
 }
